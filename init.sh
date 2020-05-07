9285b75af5aad174ab2c99d31d2de06290410bfa
mdinos <32902031+mdinos@users.noreply.github.com>
2020-05-07 12:42:39 +0100

    Include pure-data installation steps in init.sh

--- init.sh
+++ init.sh
@@ -2,4 +2,9 @@
 
 echo "Cloning pure-data repo and installing python packages..."
 git clone git@github.com:pure-data/pure-data.git
+cd pure-data
+./autogen.sh
+./configure
+make
+cd -
 pip install -r requirements.txt
